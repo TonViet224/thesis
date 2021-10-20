7844ab94953f21b1d014d92b6bcf83ff920cb9c8
TonViet224 <vietton224@gmail.com>
2021-10-20 15:24:43 +0700

    changes to my canvas

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,18 @@
-import { useState,useEffect, Suspense } from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { useState, useEffect, Suspense } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './Component/Header'
 import Tasks from './Component/Tasks'
 import AddTask from './Component/AddTask'
 import { Canvas } from '@react-three/fiber';
 import Box from './Component/Box'
+import MyCanvas from './Component/MyCanvas'
 import { Environment, OrbitControls } from "@react-three/drei";
 import Model from './Component/Model'
 import Footer from './Component/Footer'
 import About from './Component/About'
 
 function App() {
-  const [showAddTask, setShowAddTask] = useState(false)  
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([])
 
   const fetchTasks = async () => {
@@ -26,8 +27,8 @@ function App() {
     return data
   }
 
-  useEffect(()=>{
-    const getTasks = async() =>{
+  useEffect(() => {
+    const getTasks = async () => {
       const tasksFromServer = await fetchTasks()
       setTasks(tasksFromServer)
     }
@@ -35,11 +36,11 @@ function App() {
     getTasks()
   }, [])
 
-  const addTask = async(task) => {
-    const res = await fetch('http://localhost:5000/tasks',{
-      method:'POST',
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
       headers: {
-        'Content-type':'application/json'
+        'Content-type': 'application/json'
       },
       body: JSON.stringify(task)
     })
@@ -52,21 +53,21 @@ function App() {
     // setTasks([...tasks, newTask])
   }
 
-  const deleteTask = async(id) => {
+  const deleteTask = async (id) => {
     await fetch(`http://localhost:5000/tasks/${id}`, {
-      method:'DELETE'
+      method: 'DELETE'
     })
     setTasks(tasks.filter((task) => task.id !== id))
 
   }
 
-  const toggleRemider = async(id) => {
+  const toggleRemider = async (id) => {
     const taskToToggle = await fetchTask(id)
-    const updateTask = {...taskToToggle, reminder: !taskToToggle.reminder}
-    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
-      method:'PUT',
-      headers:{
-        'Content-type':'application/json'
+    const updateTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
       },
       body: JSON.stringify(updateTask)
     })
@@ -80,41 +81,35 @@ function App() {
 
   return (
     <Router>
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)}
-      showAdd = {showAddTask}/>
-      <Route path='/' exact render={(props)=>(
-      <>
-      {showAddTask &&<AddTask onAdd={addTask}/>}
-      {tasks.length > 0 ? (
-        < Tasks
-          tasks={tasks}
-          onDelete={deleteTask}
-          onToggle={toggleRemider}
-        />
-      ) : (
-        'Empty Note'
-      )}
-      <Canvas>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10 - 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[2.5, 0, 0]} />
-      </Canvas>
-      <div className='App'>
-        <Canvas>
-          <Suspense fallback={null}>
-            <Model />
-            <OrbitControls />
-            <Environment preset='sunset' background />
-          </Suspense>
-        </Canvas>
-      </div>
-      </>)}  />
-      <Route path='/about' component={About} />
-      <Footer />
-    </div >
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask} />
+        <Route path='/' exact render={(props) => (
+          <>
+            {showAddTask && <AddTask onAdd={addTask} />}
+            {tasks.length > 0 ? (
+              < Tasks
+                tasks={tasks}
+                onDelete={deleteTask}
+                onToggle={toggleRemider}
+              />
+            ) : (
+              'Empty Note'
+            )}
+            <MyCanvas />
+            <div className='App'>
+              <Canvas>
+                <Suspense fallback={null}>
+                  <Model />
+                  <OrbitControls />
+                  <Environment preset='sunset' background />
+                </Suspense>
+              </Canvas>
+            </div>
+          </>)} />
+        <Route path='/about' component={About} />
+        <Footer />
+      </div >
     </Router>
   )
 }
