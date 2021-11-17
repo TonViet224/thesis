44d08c041d5305e7ddec430dc5e1515288728559
viet quoc <vietton224@gmail.com>
2021-11-17 07:38:45 +0700

    update panolen

--- my-app/src/Component/Panoramic.js
+++ my-app/src/Component/Panoramic.js
@@ -1,7 +1,30 @@
-import Iframe from 'react-iframe'
+import { Suspense, useState } from "react"
+import { Canvas, useLoader } from "@react-three/fiber"
+import { OrbitControls, Preload } from "@react-three/drei"
+import * as THREE from 'three'
+
+const Dome = () => {
+    const texture = useLoader(THREE.TextureLoader, './img360/pic1.jpg')
+    return (
+        <mesh>
+            <sphereBufferGeometry args={[500, 60, 40]} />
+            <meshBasicMaterial map={texture} side={THREE.BackSide} />
+        </mesh>
+    )
+}
+
 const Panoramic = () => {
     return (
-        <iframe width="1000px" height="1000px" allowfullscreen frameborder="0" src="https://orbix360.com/t/IEhiElpd3uX6ASwW02SKch4bhu23/5992146897207296/demo?embed=yes"></iframe>
+        /*<iframe width="1000px" height="1000px" allowfullscreen frameborder="0" src="https://orbix360.com/t/IEhiElpd3uX6ASwW02SKch4bhu23/5992146897207296/demo?embed=yes"></iframe>*/
+        <div className='canvas3d'>
+            <Canvas frameloop="demand" camera={{ position: [0, 0, 0.1] }}>
+                <OrbitControls enableZoom={false} enablePan={false} enableDamping dampingFactor={0.2} autoRotate={false} rotateSpeed={-0.5} />
+                <Suspense fallback={null}>
+                    <Dome/>
+                    <Preload all/>
+                </Suspense>
+            </Canvas>
+        </div>
     )
 }
 export default Panoramic
