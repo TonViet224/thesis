1d5a0035d9fc387914d3c2569d38280d1367c244
viet quoc <vietton224@gmail.com>
2021-11-23 15:38:26 +0700

    add feature to panolen

--- my-app/src/Component/Panoramic.js
+++ my-app/src/Component/Panoramic.js
@@ -1,16 +1,37 @@
 import { Suspense, useState } from "react"
 import { Canvas, useLoader } from "@react-three/fiber"
-import { OrbitControls, Preload } from "@react-three/drei"
+import { OrbitControls, Preload, Html } from "@react-three/drei"
 import * as THREE from 'three'
 
-const Dome = () => {
-    const texture = useLoader(THREE.TextureLoader, './img360/pic1.jpg')
+const store = [
+    { name: 'corner 1', position: [15, -5, 1], url: './img360/panorama_1.png', link: 1 },
+    { name: 'corner 2', position: [-15, -5, 1], url: './img360/panorama_2.png', link: 0 }
+]
+const Dome = ({ name, position, texture, onClick }) => {
+    /*const texture = useLoader(THREE.TextureLoader, './img360/panorama_2.png')*/
     return (
-        <mesh>
-            <sphereBufferGeometry args={[500, 60, 40]} />
-            <meshBasicMaterial map={texture} side={THREE.BackSide} />
-        </mesh>
+        <group>
+            <mesh>
+                <sphereBufferGeometry args={[500, 60, 40]} />
+                <meshBasicMaterial map={texture} side={THREE.BackSide} />
+            </mesh>
+            <mesh position={position}>
+                <sphereGeometry args={[0.75, 12, 12]} />
+                <meshBasicMaterial color="white" />
+                <Html center>
+                    <button onClick={onClick}>go to new corner</button>
+                </Html>
+            </mesh>
+        </group>
+
     )
+}
+
+function Portals() {
+    const [which, set] = useState(0)
+    const { link, ...props } = store[which]
+    const maps = useLoader(THREE.TextureLoader, store.map((entry) => entry.url)) // prettier-ignore
+    return <Dome onClick={() => set(link)} {...props} texture={maps[which]} />
 }
 
 const Panoramic = () => {
@@ -20,8 +41,8 @@ const Panoramic = () => {
             <Canvas frameloop="demand" camera={{ position: [0, 0, 0.1] }}>
                 <OrbitControls enableZoom={false} enablePan={false} enableDamping dampingFactor={0.2} autoRotate={false} rotateSpeed={-0.5} />
                 <Suspense fallback={null}>
-                    <Dome/>
-                    <Preload all/>
+                    <Preload all />
+                    <Portals />
                 </Suspense>
             </Canvas>
         </div>
