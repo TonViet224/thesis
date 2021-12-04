20eb995b98d1cbbd6becc618acb0d8ead686b326
viet quoc <vietton224@gmail.com>
2021-12-04 09:18:05 +0700

    improve annotation

--- my-app/src/Component/Annotation.js
+++ my-app/src/Component/Annotation.js
@@ -1,10 +1,20 @@
+import React, { Suspense, useRef, useState } from 'react'
 import { Html } from '@react-three/drei'
-import './css/Annotation.module.css'
+import style from './css/Annotation.module.css'
+/*<p>{content}</p>*/
 const Annotation = ({ pos, content }) => {
+    const [hidden, setVisible] = useState(false)
     return (
-        <Html distanceFactor={20} position={pos} >
-            <div className="content">
-                <p>{content}</p>
+        <Html
+            distanceFactor={20}
+            position={pos}
+            style={{ transition: 'all 0.2s', opacity: hidden ? 0.5 : 1 }}
+            center
+            sprite
+            occlude
+            onOcclude={setVisible}>
+            <div className={style.annotationMarker}>
+                1
             </div>
         </Html>
     )
--- my-app/src/Component/Model.js
+++ my-app/src/Component/Model.js
@@ -1,26 +1,7 @@
 import React, { Suspense, useRef } from 'react'
 import { useLoader, useFrame } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { Html } from '@react-three/drei'
 import Annotation from './Annotation'
-/*<Html distanceFactor={10} position={[0, -0.1, 0]}>
-                <div class="content">
-                    hello <br />
-                    world 1
-                </div>
-            </Html>
-            <Html distanceFactor={10} position={[0, 5, 0]}>
-                <div class="content">
-                    hello <br />
-                    world 2
-                </div>
-            </Html>
-            <Html distanceFactor={10} position={[2, 0, 0]}>
-                <div class="content">
-                    hello <br />
-                    world 3
-                </div>
-            </Html> */
 const Model = ({ onAni, url3d, scale }) => {
     const gltf = useLoader(GLTFLoader, url3d)
     const ref = useRef()
@@ -35,7 +16,7 @@ const Model = ({ onAni, url3d, scale }) => {
                 <primitive position={[0, 0, 0]} object={gltf.scene} scale={scale} />
             </mesh>
             <Annotation pos={[0, -0.1, -2]} content={'hello world 1'} />
-            <Annotation pos={[0, 7, -1]} content={'hello world 2'} />
+            <Annotation pos={[-1, 7, -1]} content={'hello world 2'} />
             <Annotation pos={[4, 4, 4]} content={'hello world 3'} />
         </>
     )
