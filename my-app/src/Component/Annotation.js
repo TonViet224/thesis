5a193c3d3c79d4104dfc7796f2130cd1d8df4f1c
viet quoc <vietton224@gmail.com>
2021-12-07 10:42:22 +0700

    add annotation zoom in

--- my-app/src/Component/Annotation.js
+++ my-app/src/Component/Annotation.js
@@ -4,6 +4,21 @@ import { useFrame } from '@react-three/fiber'
 import style from './css/Annotation.module.css'
 import * as THREE from 'three'
 
+// initial camera position
+const cameraTarget = new THREE.Vector3(0, 15, 30)
+
+/**
+ * return new camera distance
+ * @param {*} pos 
+ * @param {*} dis 
+ * @returns 
+ */
+const zoompos = (pos, dis) => {
+    const a = pos;
+    const da = Math.sqrt(Math.pow(a[0], 2) + Math.pow(a[1], 2) + Math.pow(a[2], 2))
+    const k = (da + dis) / da
+    return [a[0] * k, a[1] * k, a[2] * k]
+}
 
 /**
  * Annotation
@@ -11,23 +26,21 @@ import * as THREE from 'three'
  * @returns 
  */
 const Annotation = ({ pos, content, anoNum }) => {
-    /*const [vec, setVec] = useState(new THREE.Vector3(0, 15, 30))*/
-
     const ref = useRef()
     const [hidden, setVisible] = useState(false)
     const [isClicked, setIsClicked] = useState(false)
 
     const AnnoZoom = (pos) => {
-        setIsClicked(!isClicked)
-        /*const anno_vec = new THREE.Vector3(pos[0], pos[1], pos[2])
-        setVec(anno_vec)*/
+        setIsClicked(true)
+        const c = zoompos(pos, 10)
+        cameraTarget.set(c[0], c[1], c[2])
     }
 
     useEffect(() => {
         const checkClickedOutSide = e => {
             if (isClicked && ref.current && !ref.current.contains(e.target)) {
                 setIsClicked(false)
-                /*setVec(new THREE.Vector3(0, 15, 30))*/
+                /*cameraTarget.set(0, 15, 30)*/
             }
 
         }
@@ -37,10 +50,11 @@ const Annotation = ({ pos, content, anoNum }) => {
         }
     }, [isClicked])
 
-    /*useFrame(({ camera }) => {
-        camera.position.lerp(vec, 0.1)
-    })*/
-
+    useFrame(({ camera }) => {
+        if (isClicked) {
+            camera.position.lerp(cameraTarget, 0.1)
+        }
+    })
     return (
         <Html
             distanceFactor={20}
